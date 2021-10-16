c7b0801a4300b38dc01984f0243f075cdae33048
Robert Brzoskowski <s21162@pjwstk@.edu.pl>
2021-10-16 11:29:04 +0200

    sda

--- src/main/java/pl/pjatk/car/CarApplication.java
+++ src/main/java/pl/pjatk/car/CarApplication.java
@@ -8,6 +8,13 @@ public class CarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarApplication.class, args);
+		print();
 	}
 
+	public static void print() {
+		String value = null;
+		Car honda = new Car("honda", "civic",  "red",  2005, 4);
+		System.out.println(honda);
+	}
 }
+
---
+++ src/main/java/pl/pjatk/car/CarService.java
@@ -0,0 +1,17 @@
+package pl.pjatk.car;
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarService {
+    public List<Car> findAll(){
+        List<Car> CarList = new ArrayList<>();
+        Car car = new Car("honda", "civic",  "red",  2005);
+        CarList.add(car);
+        return CarList;
+    }
+}
